4409b91fdbbd89287214dff706c9e56b09b3923f
kucing kalem <frosi551@gmail.com>
2025-12-26 21:15:07 +0700

    revision height static to dynamic in about page

--- src/components/about/section-contact.tsx
+++ src/components/about/section-contact.tsx
@@ -3,7 +3,7 @@ import Peta from "./contact/peta";
 
 export default function ContactSection() {
   return (
-    <div className="container-layout lg:h-dvh flex items-center mb-10 lg:mb-0">
+    <div className="container-layout lg:min-h-dvh py-24 flex items-center mb-10 lg:mb-0">
       <div className="flex flex-col-reverse md:flex-row px-5 py-10 lg:px-15 lg:py-20 gap-16 bg-light-primary rounded-[1.875rem] w-full" id="contact">
         <ContactForm />
         <Peta />
